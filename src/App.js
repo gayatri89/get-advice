e8c311850ef049265bf4b69a55e449c5de364549
Gayatri <gayatri2889@gmail.com>
2024-10-05 10:28:07 +0000

    set count for numner of advices per day

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function App() {
   const [data, setData] = useState(null);
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
     const apiUrl = "https://api.adviceslip.com/advice";
@@ -9,12 +10,16 @@ export default function App() {
     const response = await fetch(apiUrl);
     const data = await response.json();
     setData(data.slip.advice);
+    setCount((c) => c + 1);
   }
 
   return (
     <div className="App">
       <h1>{data && data}</h1>
       <button onClick={getAdvice}>Get advice</button>
+      <p>
+        You have read <strong>{count}</strong> pieces of advice
+      </p>
     </div>
   );
 }
